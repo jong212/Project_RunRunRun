fee95673fa467037bb6a68ad54e0b4184fb521ed
jonghwa <jonghwa0212@gmail.com>
2024-06-18 02:20:06 +0900

    [temp] 게임씬 임시 오브젝트 추가

--- Assets/script/CreateAndJoin.cs
+++ Assets/script/CreateAndJoin.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 
 public class CreateAndJoin : MonoBehaviourPunCallbacks
 {
     public TMP_InputField input_Create;
     public TMP_InputField input_Join;
-    
+
     public void CreateRoom()
     {
-        PhotonNetwork.CreateRoom(input_Create.text);
+        //PhotonNetwork.JoinRandomOrCreateRoom(null, 4);
+        RoomOptions roomOptions = new RoomOptions() { MaxPlayers = 8, IsVisible = true, IsOpen = true };
+        PhotonNetwork.CreateRoom(input_Create.text, roomOptions, TypedLobby.Default, null);
     }
     public void JoinRoom()
     {
@@ -21,5 +24,6 @@ public class CreateAndJoin : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         PhotonNetwork.LoadLevel("GamePlay");
+        print("방에 참가한 플레이어 인원 수 " + PhotonNetwork.CountOfPlayersInRooms);
     }
 }
